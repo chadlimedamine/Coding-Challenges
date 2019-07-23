041403c6c24a62c8b8faccfa0640811b378b9aee
Amine Chadli <chadliamino@gmail.com>
2019-07-23 14:18:46 +0100

    Create check_nums.py

---
+++ check_nums.py
@@ -0,0 +1,13 @@
+def CheckNums(num1,num2): 
+
+    # code goes here 
+    if num2 > num1:
+        return True
+    elif num2 < num1:
+        return False
+    else:
+        return "-1"
+
+    
+# keep this function call here  
+print CheckNums(raw_input())
